f7f18879315bc46cf6db35243ae3982ee333e1f6
smoothpie <kupriyanovichk72@gmail.com>
2024-01-28 19:42:04 +0400

    Small fixes

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <div className={styles.links}>
         <a href="https://www.chillsubs.com">Chill Subs</a>
         {/* <a href="https://teal.tools">Relocate me</a> */}
-        <a href="https://linkedin.com/karina-kupp">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/karina-kupp/">LinkedIn</a>
         <a href="https://instagram.com/karinakupp">Instagram</a>
         {/* <a href="https://instagram.com/karinakupp">Let's be friends please</a> */}
       </div>
