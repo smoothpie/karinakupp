24235ce42ba2eed5ba1c90a15763f7cbed169bbd
smoothpie <kupriyanovichk72@gmail.com>
2024-01-31 22:00:54 +0400

    Burger menu

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -19,6 +19,21 @@ export default function Navbar() {
         {/* <Link href="/favorites">Favorites</Link>
         <Link href="/misc">Misc</Link> */}
       </div>
+
+      <div className={styles.burgerMenuContainer}>
+        <input type="checkbox" />
+        
+        <span></span>
+        <span></span>
+        <span></span>
+        
+        <ul className={styles.burgerMenu}>
+          <Link href="/"><li>Home</li></Link>
+          <Link href="/about"><li>About</li></Link>
+          <Link href="/blog"><li>Blog</li></Link>
+          <Link href="/projects"><li>Projects</li></Link>
+        </ul>
+      </div>
     </nav>
   )
 }
